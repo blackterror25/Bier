2e3220c5a7e7806885f567d8eade56905f36d88d
blackterror25 <Benborra@hotmail.com>
2016-09-26 17:55:12 +0200

    Add project files.

---
+++ Bier/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bier.Startup))]
+namespace Bier
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

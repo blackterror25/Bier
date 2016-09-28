27c08b84857e52a4fffa9e88447a428a0e3facff
blackterror25 <Benborra@hotmail.com>
2016-09-28 16:08:40 +0200

    Update bootstrap en begin aanmaken models

--- Bier/App_Start/FilterConfig.cs
+++ Bier/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace Bier
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
